bd8181679fd8898295291fe2b3162aaf97712982
orjands <120662421+orjands@users.noreply.github.com>
2023-12-18 17:30:24 +0100

    Revert "Second Push"
    This reverts commit 7ab9c02dadc69e135c08f492d800e9abc9cb3a45.

--- marker_detection_RGB/calibration_data/read_npz_file.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-loaded_arrays = np.load("MultiMatrix.npz")
-
-# print(loaded_arrays.files)
-print("camMatrix:")
-print(loaded_arrays["camMatrix"])
-print("distCoef:")
-print(loaded_arrays["distCoef"])
